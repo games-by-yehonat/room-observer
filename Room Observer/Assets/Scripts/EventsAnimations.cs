3477edab1b97490dd4fb42e54482271e6d38a1d9
DESKTOP-BJ7D42A\Jonatan <jjonatansosa@gmail.com>
2021-05-06 10:56:52 -0500

    finish logic

---
+++ Room Observer/Assets/Scripts/EventsAnimations.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EventsAnimations : MonoBehaviour
+{
+    [SerializeField] private UnityEvent[] eventToRaise;
+    
+    public void RunEventRaise(int eventIndex)
+    {
+        eventToRaise[eventIndex]?.Invoke();
+    }
+}
---
+++ Room Observer/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        var sceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(sceneName);
+    }
+}
